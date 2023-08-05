d5ecede8f474f88a315b62cf47b3f24deb9bd208
Yamuna Raja <webbyperso@gmail.com>
2023-08-06 00:02:46 +0530

    imported qr-image package

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import inquirer from "inquirer";
+import qr from "qr-image";
 
 inquirer
   .prompt([
     {message: "Type the full url here to get the qr code:",
-    name: "qr_url";
+    name: "qr_url"
   }
   ])
   .then((answers) => {
-    // Use user feedback for... whatever!!
+    const qr_code = answers.qr_url;
+    console.log(qr_code);
   })
   .catch((error) => {
     if (error.isTtyError) {
