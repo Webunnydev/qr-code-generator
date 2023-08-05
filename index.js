3b66eee34a03c15cca3b3662a6652371ab54f497
Yamuna Raja <webbyperso@gmail.com>
2023-08-05 23:57:34 +0530

    creating objects

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 import inquirer from "inquirer";
+
+inquirer
+  .prompt([
+    {message: "Type the full url here to get the qr code:",
+    name: "qr_url";
+  }
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
